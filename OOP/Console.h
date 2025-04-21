f292b0232e7b05bd378ad89b4281f456f224326b
Tristin <TCturtle16@outlook.com>
2025-04-21 12:21:05 -0600

    Fixed problem for running code

--- OOP/Console.h
+++ OOP/Console.h
@@ -2,38 +2,38 @@
 #include <iostream>
 #include <limits>
 // Function that safely reads an int from cin with validation
-int getValidatedInt() {
-    int x;
-    while (true) {
-        std::cin >> x;
-
-        if (std::cin.fail()) {
-            std::cin.clear(); // clear the fail state
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // discard invalid input
-            std::cout << "Invalid input. Please enter a valid number.\n";
-        }
-        else {
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // flush any extra input
-            return x;
-        }
-    }
-}
-float getValidatedFloat() {
-    float x;
-    while (true) {
-        std::cin >> x;
-
-        if (std::cin.fail()) {
-            std::cin.clear(); // clear the fail state
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // discard invalid input
-            std::cout << "Invalid input. Please enter a valid number.\n";
-        }
-        else {
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // flush any extra input
-            return x;
-        }
-    }
-}
+//int getValidatedInt() {
+//    int x;
+//    while (true) {
+//        std::cin >> x;
+//
+//        if (std::cin.fail()) {
+//            std::cin.clear(); // clear the fail state
+//            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // discard invalid input
+//            std::cout << "Invalid input. Please enter a valid number.\n";
+//        }
+//        else {
+//            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // flush any extra input
+//            return x;
+//        }
+//    }
+//}
+//float getValidatedFloat() {
+//    float x;
+//    while (true) {
+//        std::cin >> x;
+//
+//        if (std::cin.fail()) {
+//            std::cin.clear(); // clear the fail state
+//            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // discard invalid input
+//            std::cout << "Invalid input. Please enter a valid number.\n";
+//        }
+//        else {
+//            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // flush any extra input
+//            return x;
+//        }
+//    }
+//}
 template <typename T>
 T getData() {
     T x;
